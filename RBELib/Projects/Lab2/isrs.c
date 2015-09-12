54d9c06845c97021ec2ead144eeaaea46a388f6e
QSSR <peter.raspe@gmail.com>
2015-09-12 15:14:46 -0400

    really separated c-files this time... I swear

---
+++ RBELib/Projects/Lab2/isrs.c
@@ -0,0 +1,24 @@
+//Globals
+//Timer0
+volatile unsigned long counter = 0;
+volatile unsigned long millisecond = 0;
+//ADC
+volatile int ADCval = 0;
+
+//Timer ISR
+ISR(TIMER0_OVF_vect){
+  //Using a prescaler of 8
+  // ((18432000 / 8) / 256) / 1000 = 9 interrupts in a ms
+  counter++;
+  if(counter >= 9){
+    millisecond++;
+    counter = 0;
+  }
+}
+
+//ADC ISR
+ISR(ADC_vect){
+
+  //Combine the two values into a 10-bit result
+  ADCval = ADCW;
+}
